07b05c2fdbab1bcaaf6a42abb1b410816dd78abc
Alessio Deiana <adeiana@gmail.com>
2014-09-14 16:07:38 +0200

    Simplify parsing in tests

--- tests/test_smarty_grammar.py
+++ tests/test_smarty_grammar.py
@@ -1,115 +1,99 @@
+"""
+It's easy to screw up other rules when modifying the underlying grammar.
+These unit tests test various smarty statements, to make refactoring the grammar more sane.
+"""
 import smartytotwig
 
 from smartytotwig.tree_walker import TreeWalker
 
 
-"""
-It's easy to screw up other rules when modifying the underlying grammar.
-These unit tests test various smarty statements, to make refactoring the grammar more sane.
-"""
+def convert_code(smarty_code):
+    ast = smartytotwig.parse_string(smarty_code)
+    tree_walker = TreeWalker(ast)
+    return tree_walker.code
+
+
+def test_print_symbol():
+    r = convert_code("{$foo}")
+    assert r == "{{ foo }}"
 
 
 def test_if_statement():
     """Test an if statement (no else or elseif)"""
-    ast = smartytotwig.parse_string(
-        "{if !foo or foo.bar or foo|bar:foo['hello']}\nfoo\n{/if}")
-    tree_walker = TreeWalker(ast)
-
-    assert tree_walker.code == "{% if not foo or foo.bar or foo|bar(foo['hello']) %}\nfoo\n{% endif %}"
+    r = convert_code("{if !foo or foo.bar or foo|bar:foo['hello']}\nfoo\n{/if}")
+    assert r == "{% if not foo or foo.bar or foo|bar(foo['hello']) %}\nfoo\n{% endif %}"
 
 
 def test_if_statement2():
     """Test an an if with an else and a single logical operation."""
-    ast = smartytotwig.parse_string("{if foo}\nbar\n{else}\nfoo{/if}")
-    tree_walker = TreeWalker(ast)
-
-    assert tree_walker.code == "{% if foo %}\nbar\n{% else %}\nfoo{% endif %}"
+    r = convert_code("{if foo}\nbar\n{else}\nfoo{/if}")
+    assert r == "{% if foo %}\nbar\n{% else %}\nfoo{% endif %}"
 
 
 def test_if_statement3():
     """Test an an if with an else and an elseif and two logical operations."""
-    ast = smartytotwig.parse_string(
+    r = convert_code(
         "{if foo and awesome.string|banana:\"foo\\\" $a\"}\nbar\n{elseif awesome.sauce[1] and blue and 'hello'}\nfoo{/if}")
-    tree_walker = TreeWalker(ast)
-
-    assert tree_walker.code == "{% if foo and awesome.string|banana(\"foo\\\" %s\"|format(a)) %}\nbar\n{% elseif awesome.sauce[1] and blue and \'hello\' %}\nfoo{% endif %}"
+    assert r == "{% if foo and awesome.string|banana(\"foo\\\" %s\"|format(a)) %}\nbar\n{% elseif awesome.sauce[1] and blue and \'hello\' %}\nfoo{% endif %}"
 
 
 def test_if_statement4():
     """Test an if with an elseif and else clause."""
-    ast = smartytotwig.parse_string(
+    r = convert_code(
         "{if foo|bar:3 or !foo[3]}\nbar\n{elseif awesome.sauce[1] and blue and 'hello'}\nfoo\n{else}bar{/if}")
-    tree_walker = TreeWalker(ast)
-
-    assert tree_walker.code == "{% if foo|bar(3) or not foo[3] %}\nbar\n{% elseif awesome.sauce[1] and blue and 'hello' %}\nfoo\n{% else %}bar{% endif %}"
+    assert r == "{% if foo|bar(3) or not foo[3] %}\nbar\n{% elseif awesome.sauce[1] and blue and 'hello' %}\nfoo\n{% else %}bar{% endif %}"
 
 
 def test_if_statement5():
     """Test an an if statement with parenthesis."""
-    ast = smartytotwig.parse_string(
+    r = convert_code(
         "{if (foo and bar) or foo and (bar or (foo and bar))}\nbar\n{else}\nfoo{/if}")
-    tree_walker = TreeWalker(ast)
-
-    assert tree_walker.code == "{% if (foo and bar) or foo and (bar or (foo and bar)) %}\nbar\n{% else %}\nfoo{% endif %}"
+    assert r == "{% if (foo and bar) or foo and (bar or (foo and bar)) %}\nbar\n{% else %}\nfoo{% endif %}"
 
 
 def test_if_statement6():
     """Test an an elseif statement with parenthesis."""
-    ast = smartytotwig.parse_string(
+    r = convert_code(
         "{if foo}\nbar\n{elseif (foo and bar) or foo and (bar or (foo and bar))}\nfoo{/if}")
-    tree_walker = TreeWalker(ast)
-
-    assert tree_walker.code == "{% if foo %}\nbar\n{% elseif (foo and bar) or foo and (bar or (foo and bar)) %}\nfoo{% endif %}"
+    assert r == "{% if foo %}\nbar\n{% elseif (foo and bar) or foo and (bar or (foo and bar)) %}\nfoo{% endif %}"
 
 
 def test_function_statement():
     """Test a a simple function statement."""
-    ast = smartytotwig.parse_string("{foo arg1=bar arg2=3}")
-    tree_walker = TreeWalker(ast)
-
-    assert tree_walker.code == "{{['arg1': bar, 'arg2': 3]|foo}}"
+    r = convert_code("{foo arg1=bar arg2=3}")
+    assert r == "{{['arg1': bar, 'arg2': 3]|foo}}"
 
 
 def test_function_statement2():
     """Test a a simple function statement with object and array arguments."""
-    ast = smartytotwig.parse_string(
+    r = convert_code(
         "{foo arg1=bar[1] arg2=foo.bar.foo arg3=foo.bar[3] arg4=foo.bar.awesome[3] }")
-    tree_walker = TreeWalker(ast)
-
-    assert tree_walker.code == "{{['arg1': bar[1], 'arg2': foo.bar.foo, 'arg3': foo.bar[3], 'arg4': foo.bar.awesome[3]]|foo}}"
+    assert r == "{{['arg1': bar[1], 'arg2': foo.bar.foo, 'arg3': foo.bar[3], 'arg4': foo.bar.awesome[3]]|foo}}"
 
 
 def test_function_statement3():
     """Test a function statement with modifiers in in the parameters."""
-    ast = smartytotwig.parse_string(
+    r = convert_code(
         "{foo arg1=bar[1]|modifier arg2=foo.bar.foo arg3=foo.bar[3]|modifier:array[0]:\"hello $foo \" arg4=foo.bar.awesome[3]|modifier2:7:'hello':\"hello\":\"`$apple.banana`\"}")
-    tree_walker = TreeWalker(ast)
-
-    assert tree_walker.code == "{{[\'arg1\': bar[1]|modifier, \'arg2\': foo.bar.foo, \'arg3\': foo.bar[3]|modifier(array[0], \"hello %s \"|format(foo)), \'arg4\': foo.bar.awesome[3]|modifier2(7, \'hello\', \"hello\", \"%s\"|format(apple.banana))]|foo}}"
+    assert r == "{{[\'arg1\': bar[1]|modifier, \'arg2\': foo.bar.foo, \'arg3\': foo.bar[3]|modifier(array[0], \"hello %s \"|format(foo)), \'arg4\': foo.bar.awesome[3]|modifier2(7, \'hello\', \"hello\", \"%s\"|format(apple.banana))]|foo}}"
 
 
 def test_for_statement():
     """Test a a simple foreach statement."""
-    ast = smartytotwig.parse_string(
+    r = convert_code(
         "{foreach item=bar from=foo }{/foreach}")
-    tree_walker = TreeWalker(ast)
-
-    assert tree_walker.code == "{% for bar in foo %}{% endfor %}"
+    assert r == "{% for bar in foo %}{% endfor %}"
 
 
 def test_for_statement2():
     """Test a more complex foreach statement."""
-    ast = smartytotwig.parse_string(
+    r = convert_code(
         "{foreach item='bar'    name=snuh key=\"foobar\" from=foo[5].bar[2]|hello:\"world\":\" $hey \" }bar{/foreach}")
-    tree_walker = TreeWalker(ast)
-
-    assert tree_walker.code == "{% for bar in foo[5].bar[2]|hello(\"world\", \" %s \"|format(hey)) %}bar{% endfor %}"
+    assert r == "{% for bar in foo[5].bar[2]|hello(\"world\", \" %s \"|format(hey)) %}bar{% endfor %}"
 
 
 def test_for_statement3():
     """Test a for statement with a foreachelse clause."""
-    ast = smartytotwig.parse_string(
+    r = convert_code(
         "{foreach item='bar'    name=snuh key=\"foobar\" from=foo.bar[2]|hello:\"world\":\" $hey \" }bar{foreachelse}{if !foo}bar{/if}hello{/foreach}")
-    tree_walker = TreeWalker(ast)
-
-    assert tree_walker.code == "{% for bar in foo.bar[2]|hello(\"world\", \" %s \"|format(hey)) %}bar{% else %}{% if not foo %}bar{% endif %}hello{% endfor %}"
+    assert r == "{% for bar in foo.bar[2]|hello(\"world\", \" %s \"|format(hey)) %}bar{% else %}{% if not foo %}bar{% endif %}hello{% endfor %}"
